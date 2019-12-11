3a926d39a49d84157c841ac8a80d5e8b057ed7cb
MunkhdalaiSainbileg <dalai8888@gmail.com>
2019-12-11 11:01:36 +0000

    using js reverse method.

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 
 function reverse(str){
+  
+  if(!str || str.length < 2 || typeof str !='string'){
+     return 'please, check input';
+  }
+  
   let items = [];
   let numberOfItem = str.length-1;
-  for(let i = numberOfItem; i > 0; i--){
-     items.push[str[i]];
+  for(let i = numberOfItem; i >= 0; i--){
+     items.push(str[i]);
   }
-
-  console.log(items.toString());
-   
+    return items.join('');
 }
 
-reverse('output here');
+function reverse2(str){
+  return str.split('').reverse().join('');
+}
+
+const reverse3 = str => str.split('').reverse().join('');
+
+const reverse4 = str => [...str].reverse().join('');
+//
+reverse4('This is exmaple');
+//reverse(12.012);
